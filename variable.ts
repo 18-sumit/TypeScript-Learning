4ccc1c3c81121edab1885f032c57b318937bf3c1
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-27 14:45:02 +0530

    variable declaration

---
+++ variable.ts
@@ -0,0 +1,12 @@
+// this is how to declare a variable:
+let greetings: string = "Hello Sumit";
+
+let mynum = 6;
+
+// mynum.toLowerCase(); // It throws error that mynum is a number not a string
+
+
+console.log(greetings);
+
+// to get rid fo those sqiggly lines temporarily
+export { } 
