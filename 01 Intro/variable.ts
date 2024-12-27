67a5ad6846d666780fe52884cf463bffe8d004a2
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-27 15:27:58 +0530

    .

--- 01 Intro/variable.ts
+++ 01 Intro/variable.ts
@@ -1,14 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // this is how to declare a variable:
-var greetings = "Hello Sumit";
+let greetings: string = "Hello Sumit";
+
 console.log(greetings);
+
 // number in TS
-var mynum = 6;
+let mynum = 6;
 // mynum.toLowerCase(); // It throws error that mynum is a number not a string
-var userId = 334455.44;
-var rollNo = 55;
+
+let userId: number = 334455.44
+let rollNo = 55
 // both the ways are correct to declare , but 2nd way is type inference : no explicitly need to mention type as of 1st way
 // TS is smart enough to automatically detect it 
 // boolean :
-var isLoggedIn = false;
+
+let isLoggedIn: boolean = false
+
+
+// to get rid fo those sqiggly lines temporarily
+export { } 
