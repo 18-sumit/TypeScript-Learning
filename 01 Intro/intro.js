63b4c061a53f28db9d48a96b9c96025cb5ec7b2e
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-27 14:32:37 +0530

    moved to folder

---
+++ 01 Intro/intro.js
@@ -0,0 +1,6 @@
+var myuser = {
+    name: "Sumit",
+    age: 20,
+};
+console.log(myuser);
+// console.log(myuser.email)
---
+++ 01 Intro/intro.ts
@@ -0,0 +1,7 @@
+let myuser = {
+    name : "Sumit",
+    age : 20,
+}
+
+console.log(myuser)
+// console.log(myuser.email)
