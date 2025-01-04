0f0dd0e2684a615302c483b2bc692d011e3258d3
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-05 00:19:27 +0530

    abstract class

---
+++ 03 Pure_TS/dist/abstractClass.js
@@ -0,0 +1,32 @@
+"use strict";
+// Abstract class : It is a blueprint for other classes
+// Abstract classes cannot create object of their own
+// But they help to define class who are inheriting them
+class TakePhoto {
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        // after some calculation
+        return 8;
+    }
+}
+// this stops working in abstract class as it is not a class , it is an object made using constructor
+// const ss = new TakePhoto("Test", "test")
+// ----------*************----------------
+// but abstract class still works with class Instagram
+class Instagram extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("Sepia");
+    }
+}
+// we can make obj using class who is inheriting the abstract class(TakePhoto)
+const ss = new Instagram("Test", "test", 3);
+ss.getReelTime();
