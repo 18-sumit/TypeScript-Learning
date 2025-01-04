7b31da96fba488bf27fa71571c5eeaa97df3efb5
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-05 00:20:00 +0530

    interface : blueprint for objects

--- 03 Pure_TS/src/second.ts
+++ 03 Pure_TS/src/second.ts
@@ -1,35 +1,37 @@
-interface TakePhoto {
-    cameraMode: string
-    filter: string
-    burst: number
-}
+// Why Interface is Important ??
 
-interface Story {
-    createStory(): void
+// interface TakePhoto {
+//     cameraMode: string
+//     filter: string
+//     burst: number
+// }
 
-}
+// interface Story {
+//     createStory(): void
 
-class Instagram implements TakePhoto {
-    constructor(
-        public cameraMode: string,
-        public filter: string,
-        public burst: number
-    ) { }
-}
+// }
 
-class Youtube implements TakePhoto , Story { //what if YT also wants to implement Story
+// class Instagram implements TakePhoto {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst: number
+//     ) { }
+// }
 
-    constructor(
-        public cameraMode: string,
-        public filter: string,
-        public burst: number,
-        public short: string // if we want to add more information than interface it is allowed but we cannot do less then what is mentioned
+// class Youtube implements TakePhoto , Story { //what if YT also wants to implement Story
 
-    ) { }
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst: number,
+//         public short: string // if we want to add more information than interface it is allowed but we cannot do less then what is mentioned
 
-    // to implemnt story this actually needs to have an method with exact same name what it is in Story
-    createStory(): void {
-        console.log("Story was Created")
-    }
+//     ) { }
 
-}
+//     // to implemnt story this actually needs to have an method with exact same name what it is in Story
+//     createStory(): void {
+//         console.log("Story was Created")
+//     }
+
+// }
