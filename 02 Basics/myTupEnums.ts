c4d785557575d4046385778c57a4081126574e7f
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-04 13:00:12 +0530

    ..

--- 02 Basics/myTupEnums.ts
+++ 02 Basics/myTupEnums.ts
@@ -15,14 +15,14 @@ let tUser: [string, number, boolean];
 tUser = ["ss", 123, true]
 
 let rgb: [number, number, number] = [255, 231, 159]
-// let rgb: [number, number, number] = [255, 231, 159 , 0.5] // Not allowed to do , as we can only put 3 arrays in the array.
+// let rgb: [number, number, number] = [255, 231, 159 , 0.5] // Not allowed to do , as we can only put 3values in the array.
 
 // NOTE: IMPORTANT AND AMBIGIOUS BEHAVIOUR OF TUPLES IN TS:
 
 type user = [number, string]
 
 const newUser: user = [122, "sumit"] // this is fine as per the type it follows , 
-// const newUser : user = ["sumit " , 123]  // 1st number then string is allowed 
+// const newUser : user = ["sumit " , 123]  // error : cuz only 1st number then string is allowed 
 
 newUser[1] = "sumit singh" // this is ultimately an array so we can overwrite it's values
 // EyeCatching feature in tuple
