a8ef974439c16d8e267d7a60df7aa66dbd76fd17
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-05 00:20:10 +0530

    interface : blueprint for objects

--- 03 Pure_TS/dist/second.js
+++ 03 Pure_TS/dist/second.js
@@ -1,21 +1,29 @@
 "use strict";
-class Instagram {
-    constructor(cameraMode, filter, burst) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-    }
-}
-class Youtube {
-    constructor(cameraMode, filter, burst, short // if we want to add more information than interface it is allowed but we cannot do less then what is mentioned
-    ) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-        this.short = short;
-    }
-    // to implemnt story this actually needs to have an method with exact same name what it is in Story
-    createStory() {
-        console.log("Story was Created");
-    }
-}
+// Why Interface is Important ??
+// interface TakePhoto {
+//     cameraMode: string
+//     filter: string
+//     burst: number
+// }
+// interface Story {
+//     createStory(): void
+// }
+// class Instagram implements TakePhoto {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst: number
+//     ) { }
+// }
+// class Youtube implements TakePhoto , Story { //what if YT also wants to implement Story
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst: number,
+//         public short: string // if we want to add more information than interface it is allowed but we cannot do less then what is mentioned
+//     ) { }
+//     // to implemnt story this actually needs to have an method with exact same name what it is in Story
+//     createStory(): void {
+//         console.log("Story was Created")
+//     }
+// }
